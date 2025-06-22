0527682f674fd42bdcd43bbd9f2a20c955935da0
Sebastian Ballesteros <seb.ballesteros25@gmail.com>
2025-06-22 11:37:19 -0500

    Codecov Configuration 22

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include "mainwindow.h"
-
+#include <csignal>
 #include <QApplication>
+
+void handleSigTerm(int)
+{
+    QCoreApplication::quit();
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    std::signal(SIGTERM, handleSigTerm);
+
     MainWindow w;
     if (QCoreApplication::arguments().contains("--test-play")) {
         w.setTestMode(true);
