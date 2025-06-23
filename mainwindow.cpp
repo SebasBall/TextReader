695619c03a349e13bf9489ce4927c0ec7e14927a
Sebastian Ballesteros <seb.ballesteros25@gmail.com>
2025-06-22 20:40:51 -0500

    Cambio del texto para verificar funcionalidad de la captura

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -144,16 +144,10 @@ void MainWindow::setup_voicesList(){
 void MainWindow::setTestMode(bool enabled){
     qDebug() << "Test Started";
     if (enabled) {
-        ui->textToRead->setPlainText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. "
-                                     "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. "
-                                     "Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. "
-                                     "Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. "
-                                     "Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. "
-                                     "Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. "
-                                     "Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. "
-                                     "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; "
-                                     "Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. "
-                                     "Maecenas adipiscing ante non diam sodales hendrerit.");
+        ui->textToRead->setPlainText("Las manzanas son frutas versátiles y nutritivas que han sido cultivadas desde tiempos antiguos. "
+                                     "Su sabor puede variar desde dulce hasta ácido, y se disfrutan tanto crudas como cocidas en una gran variedad de recetas. "
+                                     "Además de ser deliciosas, son una buena fuente de fibra y antioxidantes. "
+                                     "Ya sea en una tarta, en jugo o simplemente al natural, la manzana ha sabido mantenerse como un clásico en la alimentación diaria.");
         QTimer::singleShot(1000, this, SLOT(on_playButton_clicked()));
         QTimer::singleShot(2000, this, [this]() {
             QString stateStr = "Speaker State: ";
